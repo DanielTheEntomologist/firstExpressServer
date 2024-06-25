4251043754ca4f52c1f2eb7336b211d1be757143
Daniel Borowiecki <daniel.maciej.borowiecki@gmail.com>
2024-06-25 22:28:28 +0200

    First exercise commit

--- server.js
+++ server.js
@@ -4,29 +4,43 @@ const app = express();
 
 const path = require("path");
 
-files = {
+const staticPath = path.join(__dirname, "/public");
+const viewsPath = path.join(__dirname, "/views");
+const defaultViewPath = path.join(__dirname, "/views/home.html");
+const error404Path = path.join(__dirname, "/views/error404.html");
+
+app.use(express.static(staticPath));
+
+const viewFiles = {
   index: "index.html",
   about: "about.html",
   contact: "contact.html",
   info: "info.html",
   history: "history.html",
+  home: "home.html",
+  "user/settings": "nologin.html",
+  "user/panel": "nologin.html",
 };
 
 app.use((req, res, next) => {
   res.showFile = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
+    res.sendFile(path.join(viewsPath, name));
   };
   next();
 });
 
-for (let key in files) {
+app.get("/", (req, res) => {
+  res.sendFile(defaultViewPath);
+});
+
+for (let key in viewFiles) {
   app.get("/" + String(key), (req, res) => {
-    res.showFile(files[key]);
+    res.showFile(viewFiles[key]);
   });
 }
 
-app.use((req, res) => {
-  res.status(404).send("404 not found...");
+app.use("*", (req, res) => {
+  res.status(404).sendFile(error404Path);
 });
 
 app.listen(8000, () => {
