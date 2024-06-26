cf4706f731dc3e93b165228f73cea3a8ad75db89
Daniel Borowiecki <daniel.maciej.borowiecki@gmail.com>
2024-06-26 20:51:15 +0200

    Use handlebar templates

---
+++ server.mjs
@@ -0,0 +1,71 @@
+// const express = require("express");
+import express from "express";
+import { engine } from "express-handlebars";
+import path from "path";
+// const path = require("path");
+
+const __dirname = path.resolve();
+
+// set app and express settings
+const app = express();
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", "./views");
+// set paths
+const staticPath = path.join(__dirname, "/public");
+const viewsPath = path.join(__dirname, "/views");
+const defaultViewPath = path.join(__dirname, "/views/home.hbs");
+const error404Path = path.join(__dirname, "/views/error404.hbs");
+
+app.use(express.static(staticPath));
+
+const viewFiles = {
+  index: "index.html",
+  about: "about.html",
+  contact: "contact.html",
+  info: "info.html",
+  history: "history.html",
+  home: "home.html",
+  "user/settings": "nologin.html",
+  "user/panel": "nologin.html",
+};
+const views = {
+  index: "index",
+  about: "about",
+  contact: "contact",
+  info: "info",
+  history: "history",
+  home: "home",
+  "user/settings": "nologin.hbs",
+  "user/panel": "nologin.hbs",
+};
+
+// app.use((req, res, next) => {
+//   res.showFile = (name) => {
+//     res.sendFile(path.join(viewsPath, name));
+//   };
+//   next();
+// });
+
+app.get("/", (req, res) => {
+  res.sendFile(defaultViewPath);
+});
+
+for (let key in views) {
+  app.get("/" + String(key), (req, res) => {
+    res.render(views[key], { layout: false });
+  });
+}
+
+app.get("/hello/:name", (req, res) => {
+  res.render("hello", { name: String(req.params.name), layout: false });
+});
+
+app.use("*", (req, res) => {
+  // res.sendFile(error404Path);
+  res.status(404).render("error404", { layout: false });
+});
+
+app.listen(8000, () => {
+  console.log("Server is running on port: 8000");
+});
